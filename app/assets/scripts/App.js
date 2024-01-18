7820bf655cc55f08aa64882c11475fa63b144581
Rob Bock <rbock9@gmail.com>
2024-01-18 14:27:22 -0500

    Added Javascript for header, finished mobile icon animations.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu' // 'MobileMenu' is a variable name we're creating. Matches the actual name of the class to stay organized.
+
+let mobileMenu = new MobileMenu(); // assigning this instance to a variable since we'll probably want to access it later on
 
 if (module.hot) {
     module.hot.accept()
 }
+
