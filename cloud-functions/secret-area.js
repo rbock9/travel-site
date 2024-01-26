980d240035c17bc95a02f27c002c2bf262c23201
Rob Bock <rbock9@gmail.com>
2024-01-26 12:55:02 -0500

    Added CORS Solution to test cloud function from Localhost.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,11 +1,23 @@
 exports.handler = function(event, context, callback) {
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers" : "Content-Type"
+      }
+     
+      if (event.httpMethod !== "POST") {
+        return callback(null, {
+          statusCode: 200,
+          headers,
+          body: "This was not a POST request"
+        })
+      }
+
     let myPasscode = "Meow"
 
     const secretContent = `
     <h3>Welcome to the Secret Area.</h3>
-    <p>Here we can tell you that the sky is <strong>blue</strong>, and the secret code is ${myPasscode}.</p>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and the secret code is <strong>${myPasscode}</strong>.</p>
     `
-
     let body
 
     if (event.body) {
@@ -17,11 +29,13 @@ exports.handler = function(event, context, callback) {
     if (body.password == "javascript") {
         callback(null, {
             statusCode: 200,
+            headers,
             body: secretContent
         })
     } else {
         callback(null, {
-            statusCode: 401
+            statusCode: 401,
+            headers
         })
     }
     
